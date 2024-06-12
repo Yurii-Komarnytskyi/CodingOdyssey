406696234e0839e1a72bf4d74faf79dc3dead4c2
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-06-12 19:08:34 +0200

    completed Connect4 kata

--- src/main/java/kyu5/KyuFive.java
+++ src/main/java/kyu5/KyuFive.java
@@ -1,7 +1,5 @@
 package kyu5;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,7 +12,8 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import org.junit.Test;
+
+
 
 class SimpleEncryptionQwerty {
 	// https://www.codewars.com/SimpleEncryptionQwerty/57f14afa5f2f226d7d0000f4/train/java
@@ -149,7 +148,7 @@ class SnakesLadders {
 	public SnakesLadders() {
 		players.add(new Player(1));
 		players.add(new Player(2));
-		
+
 		lifts.addAll(List.of(new Lift(2, 38), new Lift(7, 14), new Lift(8, 31), new Lift(15, 26), new Lift(16, 6),
 				new Lift(21, 42), new Lift(28, 84), new Lift(36, 44), new Lift(46, 25), new Lift(49, 11),
 				new Lift(51, 67), new Lift(62, 19), new Lift(64, 60), new Lift(71, 91), new Lift(74, 53),
@@ -158,25 +157,25 @@ class SnakesLadders {
 	}
 
 	public String play(int diceOne, int diceTwo) {
-		if(gameOver) {
+		if (gameOver) {
 			return "Game over!";
 		} else {
 			Player activePlayer = players.get(indexOfActivePlayer);
-			
+
 			activePlayer.updatePositionOnBoard(diceOne + diceTwo);
 			updIndexOfActivePlayer(diceOne == diceTwo);
-			
-			if(activePlayer.positionOnBoard == FINISH_SQUARE) {
+
+			if (activePlayer.positionOnBoard == FINISH_SQUARE) {
 				gameOver = true;
 			}
-			
+
 			return activePlayer.toString();
 		}
 	}
-	
+
 	void updIndexOfActivePlayer(boolean diceAreDouble) {
-		if(!diceAreDouble) {
-			indexOfActivePlayer = (indexOfActivePlayer < players.size()-1) ? ++indexOfActivePlayer : 0;			
+		if (!diceAreDouble) {
+			indexOfActivePlayer = (indexOfActivePlayer < players.size() - 1) ? ++indexOfActivePlayer : 0;
 		}
 	}
 
@@ -191,21 +190,22 @@ class SnakesLadders {
 
 		void updatePositionOnBoard(int diceSum) {
 			positionOnBoard += diceSum;
-			if(positionOnBoard > FINISH_SQUARE) {
+			if (positionOnBoard > FINISH_SQUARE) {
 				bounceOff();
 			}
-			if(lifts.stream().anyMatch(lift -> lift.getOrigin() == positionOnBoard)) {
-				positionOnBoard = lifts.stream().filter(l -> l.getOrigin() == positionOnBoard).findFirst().get().getDestination();	
+			if (lifts.stream().anyMatch(lift -> lift.getOrigin() == positionOnBoard)) {
+				positionOnBoard = lifts.stream().filter(l -> l.getOrigin() == positionOnBoard).findFirst().get()
+						.getDestination();
 			}
 		}
-		
+
 		void bounceOff() {
 			positionOnBoard = FINISH_SQUARE - (positionOnBoard - FINISH_SQUARE);
 		}
 
 		@Override
 		public String toString() {
-			if(this.positionOnBoard == FINISH_SQUARE) {
+			if (this.positionOnBoard == FINISH_SQUARE) {
 				return String.format("Player %d Wins!", playerId);
 			}
 			return String.format("Player %d is on square %d", playerId, positionOnBoard);
@@ -233,9 +233,157 @@ class SnakesLadders {
 
 }
 
+class Connect4 {
+	// https://www.codewars.com/kata/586c0909c1923fdb89002031/train/java
+
+	private final List<List<String>> theBoard = new ArrayList<>();
+	private final List<Player> players = new ArrayList<>();
+	List<String> currentColumn;
+	private Player activePlayer;
+	private boolean gameOver = false;
+	private final String YELLOW = "y";
+	private final String GREEN = "g";
+	private final String BLANK = " ";
+	private final String COLUMN_FULL = "Column full!";
+	private final int WINNING_STREAK = 4;
+	private final int AMOUNT_OF_COLUMNS = 7;
+	private final int COLUMN_SIZE = 6;
+
+	private class Player {
+
+		private int playerId;
+		private String discColor;
+		private boolean isActive = false;
+
+		public Player(int playerId, String discColor) {
+			this.playerId = playerId;
+			this.discColor = discColor;
+		}
+
+		private String dropADisc() {
+			int blankIndex = currentColumn.indexOf(BLANK);
+			if (blankIndex == -1) {
+				return COLUMN_FULL;
+			}
+			currentColumn.set(blankIndex, activePlayer.discColor);
+			return String.format("Player %d has a turn", playerId);
+		}
+
+		private void reverseIsActiveField() {
+			isActive = !isActive;
+		}
+
+	}
+
+	public Connect4() {
+		players.add(new Player(1, YELLOW));
+		players.add(new Player(2, GREEN));
+		activePlayer = players.get(0);
+		activePlayer.reverseIsActiveField();
+
+		IntStream.range(0, AMOUNT_OF_COLUMNS).forEach(column -> {
+			theBoard.add(column, new ArrayList<>());
+			IntStream.range(0, COLUMN_SIZE).forEach(row -> {
+				theBoard.get(column).add(BLANK);
+			});
+		});
+
+	}
+
+	public String play(int column) {
+		if (gameOver) {
+			return "Game has finished!";
+		}
+		currentColumn = theBoard.get(column);
+		String resultOfATurn = activePlayer.dropADisc();
+
+		if (checkIfActivePlayerHasWon()) {
+			gameOver = true;
+			return String.format("Player %d wins!", activePlayer.playerId);
+		} else {
+			activePlayer = updActivePlayer(resultOfATurn);
+			return resultOfATurn;
+		}
+
+	}
+
+	Player updActivePlayer(String playersResult) {
+		if (!playersResult.equals(COLUMN_FULL)) {
+			players.stream().forEach(Player::reverseIsActiveField); 
+			return players.stream().filter(player -> player.isActive == true).findFirst().get();
+		} else {
+			return activePlayer;
+		}
+	}
+
+	private boolean checkIfActivePlayerHasWon() {
+		return checkTheBoardVertically() || checkTheBoardHorizontally() || checkTheBoardDiagonally();
+	}
+
+	private boolean checkTheBoardVertically() {
+		return checkForFourConsecutiveDiscs(currentColumn);
+	}
+
+	private boolean checkTheBoardHorizontally() {
+		int horizontalIndex = currentColumn.lastIndexOf(activePlayer.discColor);
+		return checkForFourConsecutiveDiscs(theBoard.stream().map(column -> column.get(horizontalIndex)).toList());
+	}
+
+	private boolean checkTheBoardDiagonally() {
+		List<String> diagonalToTheLeft = new ArrayList<>();
+		List<String> bottomToTop = new ArrayList<>();
+		int horizontalIndex = currentColumn.lastIndexOf(activePlayer.discColor);
+		int currentColumnIndex = theBoard.indexOf(currentColumn);
+		
+		{	// BOTTOM TO TOP
+			int topRight = (currentColumnIndex + ((COLUMN_SIZE-1) - horizontalIndex) > AMOUNT_OF_COLUMNS-1)
+					? AMOUNT_OF_COLUMNS-1
+					: currentColumnIndex + ((COLUMN_SIZE-1) - horizontalIndex);
+			int bottomLeft = (currentColumnIndex - horizontalIndex < 0) ? 0
+					: currentColumnIndex - horizontalIndex;
+			int discIndex = 0;
+			for(int col = bottomLeft; col <= topRight; col++) {
+				bottomToTop.add(theBoard.get(col).get(discIndex));
+				discIndex++;
+			}
+		}
+		
+		{	// TOP TO BOTTOM
+			int bottomRight = (currentColumnIndex + horizontalIndex > AMOUNT_OF_COLUMNS-1) 
+					? AMOUNT_OF_COLUMNS - 1
+					: currentColumnIndex + horizontalIndex;
+			int topLeft = (currentColumnIndex - ((COLUMN_SIZE-1) - horizontalIndex) < 0) ? 0
+					: currentColumnIndex - ((COLUMN_SIZE-1) - horizontalIndex);
+			int discIndex = 0;
+			for(int col = bottomRight; col >= topLeft; col--) {
+				diagonalToTheLeft.add(theBoard.get(col).get(discIndex));
+				discIndex++;
+			}
+		}
+		return checkForFourConsecutiveDiscs(bottomToTop) || checkForFourConsecutiveDiscs(diagonalToTheLeft);
+	}
+
+	private boolean checkForFourConsecutiveDiscs(List<String> list) {
+		int numberOfConsecutiveDiscs = 0;
+		for (String disc : list) {
+			if (disc.equals(activePlayer.discColor)) {
+				++numberOfConsecutiveDiscs;
+				if (numberOfConsecutiveDiscs == WINNING_STREAK) {
+					break;
+				}
+			} else {
+				numberOfConsecutiveDiscs = 0;
+			}
+		}
+		return numberOfConsecutiveDiscs == WINNING_STREAK;
+	}
+}
+
+
 public class KyuFive {
 
 	public static void main(String[] args) {
+		Connect4 t = new Connect4();
 	}
 
 }
