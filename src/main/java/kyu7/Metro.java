54d2d970e8c77f7b1a14a45567553c63cbcba824
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-07-08 22:03:09 +0200

    renamed KyuSeven

--- src/main/java/kyu7/Metro.java
+++ src/main/java/kyu7/Metro.java
@@ -2,7 +2,7 @@ package kyu7;
 
 import java.util.ArrayList;
 
-class Metro {
+public class Metro {
 	// https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/java
 	public static int countPassengers(ArrayList<int[]> stops) {
 		Integer passengersInside = 0;		
@@ -11,14 +11,4 @@ class Metro {
 		}
 		return passengersInside;
 	}
-}
-
-public class KyuSeven {
-
-	public static void main(String[] args) {
-		KyuSeven kyuSeven = new KyuSeven();
-		String text = "A";
-		System.out.println(String.valueOf(text.charAt(0)));
-	}
-
 }
