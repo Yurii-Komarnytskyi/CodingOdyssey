deb5f9c989a228035c7cc2d571c5f912695912b0
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-03-06 20:39:25 +0100

    Completed kata WeightSort

---
+++ src/test/java/kyu5/WeightSortTest.java
@@ -0,0 +1,15 @@
+package kyu5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WeightSortTest {
+	@Test
+	void BasicTests() {
+		System.out.println("****** Basic Tests ******");
+		assertEquals("2000 103 123 4444 99", WeightSort.orderWeight("103 123 4444 99 2000"));
+		assertEquals("100 180 90 56 65 74 68 86 99", WeightSort.orderWeight("56 65 74 100 99 68 86 180 90"));
+		assertEquals("11 11 2000 10003 22 123 1234000 44444444 9999", WeightSort.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+	}
+}
