10377781614cd568804dec2480da62ddce3006d7
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-04-23 22:38:42 +0200

    completed easy NeetCode algorithm problems

---
+++ src/main/java/neetcode/blind75/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+package neetcode.blind75;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+	// https://neetcode.io/problems/duplicate-integer
+	public boolean hasDuplicate(int[] nums) {
+		Set<Integer> uniqueNums = new HashSet<>();
+		for(int i : nums) {
+			if(!uniqueNums.add(i)) {
+				return true;
+			}
+		}
+		return false;
+    }
+}
---
+++ src/main/java/neetcode/blind75/TwoSum.java
@@ -0,0 +1,10 @@
+package neetcode.blind75;
+
+public class TwoSum {
+
+	public int[] twoSum(int[] nums, int target) {
+		int[] indicesOfTwoSum = new int[2];
+		
+		return indicesOfTwoSum;
+	}
+}
