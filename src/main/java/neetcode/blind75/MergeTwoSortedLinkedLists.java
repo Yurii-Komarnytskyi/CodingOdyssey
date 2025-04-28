2abae1c31e64a3717be7bf06adf33011e1e55713
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-04-28 22:10:11 +0200

    completed Merge Two Sorted Linked Lists on Neetcode

---
+++ src/main/java/neetcode/blind75/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,44 @@
+package neetcode.blind75;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MergeTwoSortedLinkedLists {
+//	https://neetcode.io/problems/merge-two-sorted-linked-lists
+	
+	private List<ListNode> sortedNodes = new ArrayList<>();
+
+	public ListNode mergeTwoLists(ListNode nodeA, ListNode nodeB) {
+		if (nodeA == null) {
+			return nodeB;
+		} else if (nodeB == null) {
+			return nodeA;
+		} 
+
+		populateAndSortListNodes(List.of(nodeA, nodeB));
+		ListNode mergedHead = sortedNodes.get(0);
+		ListNode nextNode = mergedHead;
+		
+		for (Iterator iterator = sortedNodes.iterator(); iterator.hasNext();) {
+			nextNode = nextNode.next = ((ListNode) iterator.next()) ;
+		}
+		
+		return mergedHead;
+	}
+	
+	private  void populateAndSortListNodes(List<ListNode> nodes) {
+		ListNode nextListNode = null;
+		for(ListNode node : nodes) {
+			if(node != null ) {
+				sortedNodes.add(new ListNode(node.val));
+				nextListNode = node.next;
+				while (nextListNode != null) {
+					sortedNodes.add(new ListNode(nextListNode.val));
+					nextListNode = nextListNode.next;
+				}
+			}
+		}
+		sortedNodes.sort((a, b) -> Integer.compare(a.val, b.val));
+	}
+}
