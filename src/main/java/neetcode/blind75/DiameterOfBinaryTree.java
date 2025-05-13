c298156564fd6ae12e03c98eecaad67ac2f08592
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-05-13 13:08:01 +0200

    completed Diameter Of Binary Tree task on NeetCode

---
+++ src/main/java/neetcode/blind75/DiameterOfBinaryTree.java
@@ -0,0 +1,33 @@
+package neetcode.blind75;
+
+import java.util.Objects;
+
+public class DiameterOfBinaryTree {
+	// https://neetcode.io/problems/binary-tree-diameter
+
+	private int maxDiameter = 0;
+
+	public int diameterOfBinaryTree(TreeNode root) {
+		if (Objects.nonNull(root)) {
+			maxDiameter = Math.max((findMaxNodeDepth(root.left) + findMaxNodeDepth(root.right)), maxDiameter);
+			diameterOfBinaryTree(root.left);
+			diameterOfBinaryTree(root.right);
+		}
+		return maxDiameter;
+	}
+
+	private int findMaxNodeDepth(TreeNode node) {
+		boolean isNodeNull = Objects.isNull(node);
+		int depth = (isNodeNull)? 0 : 1;
+		int maxDepthOnLeft = 1;
+		int maxDepthOnRight = 1;
+		if (isNodeNull) {
+			return depth; 
+		} else {
+			maxDepthOnLeft += findMaxNodeDepth(node.left);
+			maxDepthOnRight += findMaxNodeDepth(node.right);
+			depth = Math.max(maxDepthOnLeft, maxDepthOnRight);
+		}
+		return depth;
+	}
+}
--- src/main/java/neetcode/blind75/MaximumDepthOfBinaryTree.java
+++ src/main/java/neetcode/blind75/MaximumDepthOfBinaryTree.java
@@ -22,23 +22,4 @@ public class MaximumDepthOfBinaryTree {
 		}
 		return result;
 	}
-	
-
-	public static void main(String[] args) {
-		MaximumDepthOfBinaryTree tree = new MaximumDepthOfBinaryTree();
-		
-		TreeNode n1 = new TreeNode(1);
-		TreeNode n2 = new TreeNode(2);
-		TreeNode n3 = new TreeNode(3);
-		TreeNode n4 = new TreeNode(4);
-		
-		n1.left = n2;
-		n1.right = n3;
-		n3.left = n4;
-		
-		System.out.println(tree.maxDepth(n1));
-		
-		
-		
-	}
 }
